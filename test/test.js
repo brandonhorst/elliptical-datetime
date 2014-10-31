198262033c03e80bdc6459509d22b3c8af35af6d
= <brandonhorst@gmail.com>
2014-10-31 11:06:59 -0400

    added hh:mm am time functionality

--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ var Parser = require('lacona').Parser;
 chai.use(require('sinon-chai'));
 chai.use(require('chai-datetime'));
 
-describe('datetime', function () {
+describe('date', function () {
 	var parser;
 
 	beforeEach(function () {
@@ -103,6 +103,43 @@ describe('datetime', function () {
 			.on('end', handleEnd)
 			.parse('in 5 days');
 		});
+	});
+});
 
+
+describe('time', function () {
+	var parser;
+
+	var grammar = {
+		phrases: [{
+			name: 'test',
+			root: {
+				type: 'time',
+				id: 'test'
+			}
+		}],
+		dependencies: [datetime]
+	};
+
+	beforeEach(function () {
+		parser = new Parser({sentences: ['test']});
+	});
+
+	it('handles hh:mm', function (done) {
+		var handleData = sinon.spy(function (data) {
+			var expectedDate = moment({hour: 11, minute: 3}).toDate();
+			expect(data.result.test).to.equalDate(expectedDate);
+		});
+
+		var handleEnd = function () {
+			expect(handleData).to.have.been.called.once;
+			done();
+		};
+
+		parser
+		.understand(grammar)
+		.on('data', handleData)
+		.on('end', handleEnd)
+		.parse('11:03 am');
 	});
 });
