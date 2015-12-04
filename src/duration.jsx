cf839396923a554d27aa01dc342e54bd85b09f3c
Brandon Horst <brandonhorst@gmail.com>
2015-12-04 13:54:15 -0500

    Updated durations

---
+++ src/duration.jsx
@@ -0,0 +1,102 @@
+/** @jsx createElement */
+import _ from 'lodash'
+import {createElement, Phrase} from 'lacona-phrase'
+import {Integer} from 'lacona-phrase-number'
+
+function isUnique (array) {
+  return _.uniq(array).length === array.length
+}
+
+class BaseDuration extends Phrase {
+  getValue (result) {
+    const output = {}
+    _.forEach(result, ({num, type}) => {
+      output[type] = (output[type] || 0) +  num
+    })
+    return output
+  }
+
+  filter (results) {
+    return isUnique(_.map(results, 'id'))
+  }
+
+  describe () {
+    return (
+      <repeat separator={<list items={[', and ', ' and ', ', ']} limit={1} category='conjunction' />}>
+        {this.childDescribe()}
+      </repeat>
+    )
+  }
+}
+
+class InternalDuration extends Phrase {
+  getValue ({id, type, num, multiplier = 1}) {
+    return {id, type, num: num * multiplier}
+  }
+
+  describe () {
+    return (
+      <choice limit={1}>
+        <sequence>
+          <Integer max={1} min={1} id='num' />
+          <literal text=' ' />
+          <placeholder text='time period' merge={true}>
+            <choice>
+              {this.props.type !== 'time' ? [
+                  <literal text='day' value={{id: 'days', type: 'days'}}  />,
+                  <literal text='fortnight' value={{id: 'fortnights', type: 'days', multiplier: 14}} />,
+                  <literal text='week' value={{id: 'weeks', type: 'days', multiplier: 7}} />,
+                  <literal text='month' value={{id: 'months', type: 'months'}} />,
+                  <literal text='year' value={{id: 'years', type: 'years'}} />
+              ] : null}
+              {this.props.type !== 'date' ? [
+                <literal text='hour' value={{id: 'hours', type: 'hours'}} />,
+                <literal text='minute' value={{id: 'minutes', type: 'minutes'}} />,
+                <literal text='second' value={{id: 'seconds', type: 'seconds'}} />
+              ] : null}
+            </choice>
+          </placeholder>
+        </sequence>
+        <sequence>
+          <Integer id='num' min={2} />
+          <literal text=' ' />
+          <placeholder text='time period' merge={true}>
+            <choice>
+              {this.props.type !== 'time' ? [
+                <literal text='days' value={{id: 'days', type: 'days'}}  />,
+                <literal text='fortnights' value={{id: 'fortnights', type: 'days', multiplier: 14}} />,
+                <literal text='weeks' value={{id: 'weeks', type: 'days', multiplier: 7}} />,
+                <literal text='months' value={{id: 'months', type: 'months'}} />,
+                <literal text='years' value={{id: 'years', type: 'years'}} />
+              ] : null}
+              {this.props.type !== 'date' ? [
+                <literal text='hours' value={{id: 'hours', type: 'hours'}} />,
+                <literal text='minutes' value={{id: 'minutes', type: 'minutes'}} />,
+                <literal text='seconds' value={{id: 'seconds', type: 'seconds'}} />
+              ] : null}
+            </choice>
+          </placeholder>
+        </sequence>
+      </choice>
+    )
+  }
+}
+
+
+export class DateDuration extends BaseDuration {
+  childDescribe () {
+    return <InternalDuration type='date' />
+  }
+}
+
+export class TimeDuration extends BaseDuration {
+  childDescribe () {
+    return <InternalDuration type='time' />
+  }
+}
+
+export class Duration extends BaseDuration {
+  childDescribe () {
+    return <InternalDuration />
+  }
+}
---
+++ test/time-duration.jsx
@@ -0,0 +1,49 @@
+/** @jsx createElement */
+
+import {createElement, Phrase} from 'lacona-phrase'
+import {expect} from 'chai'
+import {text} from './_util'
+import {TimeDuration} from '..'
+import {Parser} from 'lacona'
+
+describe('time-duration', () => {
+	let parser
+
+	beforeEach(() => {
+		parser = new Parser()
+		parser.grammar = <TimeDuration />
+	})
+
+	it('1 minute', () => {
+		const data = parser.parseArray('1 minute')
+		expect(data).to.have.length(2)
+		expect(text(data[0])).to.equal('1 minute')
+		expect(data[0].result).to.eql({minutes: 1})
+	})
+
+	it('x minutes', () => {
+		const data = parser.parseArray('3 minutes')
+		expect(data).to.have.length(2)
+		expect(text(data[0])).to.equal('3 minutes')
+		expect(data[0].result).to.eql({minutes: 3})
+	})
+
+	it('x hours and 1 minute', () => {
+		const data = parser.parseArray('3 hours and 1 minute')
+		expect(data).to.have.length(2)
+		expect(text(data[0])).to.equal('3 hours and 1 minute')
+		expect(data[0].result).to.eql({hours: 3, minutes: 1})
+	})
+
+	it('1 hour, x minutes, and x seconds', () => {
+		const data = parser.parseArray('1 hour, 2 minutes, and 3 seconds')
+		expect(data).to.have.length(2)
+		expect(text(data[0])).to.equal('1 hour, 2 minutes, and 3 seconds')
+		expect(data[0].result).to.eql({hours: 1, minutes: 2, seconds: 3})
+	})
+
+	it('x minutes and x minutes', () => {
+		const data = parser.parseArray('2 minutes and 3 minutes')
+		expect(data).to.have.length(0)
+	})
+})
